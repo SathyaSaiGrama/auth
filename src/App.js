abd3fcf32b2071293355ebbebcf0d76a711d8881
vivek <vivekkashyap043@gmail.com>
2024-09-22 14:36:54 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-import ResetPassword from './components/ResetPassword';
-import VerifyEmail from './components/VerifyEmail';
+import ActionHandler from './components/ActionHandler';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/" element={<ActionHandler />} />
       </Routes>
     </Router>
   );
